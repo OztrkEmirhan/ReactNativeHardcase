0f16d6156bac396d24d3f6d7937ddbbce49e6087
Emirhan_Ozturk <emirhan.ozturk@kalemyazilim.com>
2023-12-07 01:16:25 +0300

    background actions push

--- src/components/AppState.tsx
+++ src/components/AppState.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable prettier/prettier */
 import React, { useEffect, useRef, useState } from 'react';
 import { AppState, StyleSheet, Text, View } from 'react-native';
+import BackgroundService from 'react-native-background-actions';
+import { startBackgroundService, stopBackgroundService } from '../services/actions';
 
 const AppStates = () => {
   const appState = useRef(AppState.currentState);
@@ -8,18 +10,23 @@ const AppStates = () => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    const subscription = AppState.addEventListener('change', nextAppState => {
+    const subscription = AppState.addEventListener('change', async nextAppState => {
       if (appState.current.match(/inactive|background/) && nextAppState === 'active') {
         console.log('App has come to the foreground!');
+        const counterOfBackground = await stopBackgroundService();
+        console.error(counterOfBackground);
       } else if (nextAppState === 'background') {
+        startBackgroundService();
         console.log('App is in the background!');
       }
 
       appState.current = nextAppState;
       setAppStateVisible(appState.current);
-
       console.log('AppState', appState.current);
-      setCounter(prevCounter => prevCounter + 1);
+    });
+
+    BackgroundService.on("expiration", () => {
+      
     });
 
     return () => {
