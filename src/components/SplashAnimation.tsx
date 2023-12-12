243d93a231faee5998ebf7fd50e50588a6085fde
Emirhan_Ozturk <emirhan.ozturk@kalemyazilim.com>
2023-12-12 14:14:07 +0300

    splashAnimation code fix

--- src/components/SplashAnimation.tsx
+++ src/components/SplashAnimation.tsx
@@ -1,27 +1,45 @@
 /* eslint-disable prettier/prettier */
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import LottieView from 'lottie-react-native';
 import React, { useEffect, useRef } from 'react';
 import { Animated, Easing } from 'react-native';
 
-import LottieView from 'lottie-react-native';
-
 const AnimatedLottieView = Animated.createAnimatedComponent(LottieView);
 
-export const SplashAnimation = ({onAnimationComplete}: any) => {
+export const SplashAnimation = ({ onAnimationComplete }: any) => {
   const animationProgress = useRef(new Animated.Value(0));
 
   useEffect(() => {
+    const checkIfAnimationPlayed = async () => {
+      try {
+        const animationPlayed = await AsyncStorage.getItem('animationPlayed');
+        if (!animationPlayed) {
+          startAnimation();
+        } else {
+          onAnimationComplete(); // Skip animation and proceed to the next screen
+        }
+      } catch (error) {
+        console.error('Error checking animationPlayed:', error);
+      }
+    };
+
     const startAnimation = () => {
       Animated.timing(animationProgress.current, {
         toValue: 1,
         duration: 5000,
         easing: Easing.linear,
         useNativeDriver: true,
-      }).start(() => {
-        onAnimationComplete(); // Animasyon tamamlandığında, parent component'e haber veriliyor
+      }).start(async () => {
+        // Save a flag indicating that the animation has been played
+        await AsyncStorage.setItem('animationPlayed', 'true');
+        onAnimationComplete();
       });
     };
 
+    checkIfAnimationPlayed();
     const delay = setTimeout(startAnimation, 3000);
+
+    // Clean up any asynchronous tasks or timers
     return () => clearTimeout(delay);
   }, [onAnimationComplete]);
 
@@ -33,7 +51,6 @@ export const SplashAnimation = ({onAnimationComplete}: any) => {
         width: '100%',
         height: '100%',
         backgroundColor: 'white',
-        
       }}
       source={require('../assets/Lottie.json')}
       progress={animationProgress.current}
@@ -41,3 +58,69 @@ export const SplashAnimation = ({onAnimationComplete}: any) => {
     />
   );
 };
+
+/* eslint-disable prettier/prettier */
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import LottieView from 'lottie-react-native';
+// import React, { useEffect, useRef } from 'react';
+// import { Animated, Easing } from 'react-native';
+
+// const AnimatedLottieView = Animated.createAnimatedComponent(LottieView);
+
+// export const SplashAnimation = ({ navigation }: any) => {
+//   const animationProgress = useRef(new Animated.Value(0));
+
+//   useEffect(() => {
+//     const checkIfAnimationPlayed = async () => {
+//       try {
+//         const animationPlayed = await AsyncStorage.getItem('animationPlayed');
+//         if (!animationPlayed) {
+//           startAnimation();
+//         } else {
+//           // Skip animation and proceed to the login screen
+//           navigation.replace('LoginScreen'); 
+//         }
+//       } catch (error) {
+//         console.error('Error checking animationPlayed:', error);
+//       }
+//     };
+
+//     const startAnimation = () => {
+//       Animated.timing(animationProgress.current, {
+//         toValue: 1,
+//         duration: 5000,
+//         easing: Easing.linear,
+//         useNativeDriver: true,
+//       }).start(async () => {
+//         // Save a flag indicating that the animation has been played
+//         await AsyncStorage.setItem('animationPlayed', 'true');
+//         // Skip animation and proceed to the login screen
+//         navigation.replace('LoginScreen'); 
+//       });
+//     };
+
+//     const delay = setTimeout(() => {
+//       checkIfAnimationPlayed();
+//       startAnimation();
+//     }, 3000);
+
+//     // Clean up any asynchronous tasks or timers
+//     return () => clearTimeout(delay);
+//   }, [navigation]);
+
+//   return (
+//     <AnimatedLottieView
+//       // eslint-disable-next-line react-native/no-inline-styles
+//       style={{
+//         flex: 1,
+//         width: '100%',
+//         height: '100%',
+//         backgroundColor: 'white',
+//       }}
+//       source={require('../assets/Lottie.json')}
+//       progress={animationProgress.current}
+//       quality="high"
+//     />
+//   );
+// };
+
