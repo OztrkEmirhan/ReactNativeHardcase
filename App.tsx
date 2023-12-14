68258e7b5c167a7e6fb2ecba01ebee32954342bd
Emirhan_Ozturk <emirhan.ozturk@kalemyazilim.com>
2023-12-14 11:12:47 +0300

    splashscreen : code fix

--- App.tsx
+++ App.tsx
@@ -5,17 +5,21 @@ import React from 'react';
 import { ApiContextProvider } from './src/context/api';
 import Main from './src/navigations/Main';
 import Login from './src/pages/Login';
-import Splash from './src/pages/Splash';
-
+// import Splash from './src/pages/Splash';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  // const [state] = useGlobalState();
+
+  // if(state.isBooting) {
+  //   return <Splash/>;
+  // }
+
   return (
     <ApiContextProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen options={{ headerShown: false }} name="Splash" component={Splash} />
           {/* Giriş Sayfası */}
           <Stack.Screen name="Login" component={Login} />
 
@@ -30,5 +34,4 @@ const App = () => {
     </ApiContextProvider>
   );
 };
-
 export default App;
